844ae2bafaf9782302264ad63907f97a79f03653
Nikica Novakovic <novakovicnikica@gmail.com>
2017-02-27 17:37:06 +0100

    Add additional tests for LocalStorageService

--- src/app/local-storage-service/local-storage.service.spec.ts
+++ src/app/local-storage-service/local-storage.service.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
-
 import { TestBed, async, inject } from '@angular/core/testing';
+
+import { Game, GameType } from '../model';
 import { LocalStorageService } from './local-storage.service';
 
 describe('LocalStorageService', () => {
@@ -18,5 +19,39 @@ describe('LocalStorageService', () => {
 
   it('should ...', () => {
     expect(localStorage).toBeTruthy();
-  }));
+  });
+
+  it('should have saveGame method', () => {
+    spyOn(localStorage, 'saveGame');
+
+    localStorage.saveGame(GameType.HUMAN_VS_AI, new Game());
+
+    expect(localStorage.saveGame).toHaveBeenCalled();
+  });
+
+  it('should have loadGame method', () => {
+    spyOn(localStorage, 'loadGame');
+
+    localStorage.loadGame(GameType.HUMAN_VS_AI);
+
+    expect(localStorage.loadGame).toHaveBeenCalled();
+  })
+
+  it('should have saveActiveGame method', () => {
+    spyOn(localStorage, 'saveActiveGame');
+
+    localStorage.saveActiveGame(GameType.HUMAN_VS_AI);
+
+    expect(localStorage.saveActiveGame).toHaveBeenCalled();
+  });
+
+  it('should have loadActiveGame method', () => {
+    spyOn(localStorage, 'loadActiveGame');
+
+    localStorage.loadActiveGame();
+
+    expect(localStorage.loadActiveGame).toHaveBeenCalled();
+  })
 });
+
+
